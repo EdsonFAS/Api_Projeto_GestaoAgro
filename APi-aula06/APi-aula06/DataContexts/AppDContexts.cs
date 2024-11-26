17b23a1c173409f8a947ce6c87cd52094ef770ea
Edson Fernando Souza Bezerra <2022202060017@aluno.ifro.edu.br>
2024-11-26 19:50:10 -0400

    feat: add modelo base

---
+++ APi-aula06/APi-aula06/DataContexts/AppDContexts.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APi_aula06.Model;
+
+namespace APi_aula06.DataContexts
+
+{
+    public class AppDContexts : DbContext
+
+    {
+        public AppDContexts(DbContextOptions<AppDContexts> options) : base(options) { }
+
+        public DbSet<ServidorModel> Servidores { get; set; }
+    }
+}
--- APi-aula06/APi-aula06/Program.cs
+++ APi-aula06/APi-aula06/Program.cs
@@ -1,3 +1,6 @@
+using APi_aula06.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var conectionString = builder.Configuration.GetConnectionString("default");
+builder.Services.AddDbContext<AppDContexts>(options => options.UseMySql(conectionString, ServerVersion.AutoDetect(conectionString)));
 
 var app = builder.Build();
 
