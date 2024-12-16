8b9a0bdf6fa96f108d7f000455c5d4c8c8e39c0b
LM <100394745+LhuanyMotta@users.noreply.github.com>
2024-12-16 11:42:45 -0400

    Update RebanhoAlimentacaoModel.cs

--- GestaoAgro/GestaoAgro/Models/RebanhoAlimentacaoModel.cs
+++ GestaoAgro/GestaoAgro/Models/RebanhoAlimentacaoModel.cs
@@ -1,19 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoAgro.Model
 {
-    [Table("RebanhoAlimentacao")]
+    // Representa a tabela 'RebanhoAlimentacao' no banco de dados
+    [Table("RebanhoAlimentacao")]  // Mapeia a classe para a tabela 'RebanhoAlimentacao'
     public class RebanhoAlimentacaoModel
     {
-        [Column("Id")]
+        // Identificador único da relação entre rebanho e alimentação
+        [Column("Id")]  // Mapeia a propriedade 'Id' para a coluna 'Id' no banco de dados
         public int Id { get; set; }
 
-        [Column("fk_Rebanho_IdRebanho")]
+        // Chave estrangeira que faz referência ao Rebanho
+        [Column("fk_Rebanho_IdRebanho")]  // Mapeia a coluna 'fk_Rebanho_IdRebanho'
         public int IdRebanho { get; set; }
-        public virtual RebanhoModel Rebanho { get; set; }
 
-        [Column("fk_Alimentacao_IdAlimentacao")]
+        // Relacionamento com o modelo 'RebanhoModel', por meio da chave estrangeira 'IdRebanho'
+        public virtual RebanhoModel Rebanho { get; set; }  // Relacionamento de navegação
+
+        // Chave estrangeira que faz referência à Alimentação
+        [Column("fk_Alimentacao_IdAlimentacao")]  // Mapeia a coluna 'fk_Alimentacao_IdAlimentacao'
         public int IdAlimentacao { get; set; }
-        public virtual AlimentacaoModel Alimentacao { get; set; }
+
+        // Relacionamento com o modelo 'AlimentacaoModel', por meio da chave estrangeira 'IdAlimentacao'
+        public virtual AlimentacaoModel Alimentacao { get; set; }  // Relacionamento de navegação
     }
 }
