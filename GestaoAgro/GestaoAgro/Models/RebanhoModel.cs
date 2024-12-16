9bbe4c6b581408c57aa9a1e990e85e297aa9b8b8
LM <100394745+LhuanyMotta@users.noreply.github.com>
2024-12-16 11:42:56 -0400

    Update RebanhoModel.cs

--- GestaoAgro/GestaoAgro/Models/RebanhoModel.cs
+++ GestaoAgro/GestaoAgro/Models/RebanhoModel.cs
@@ -1,24 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoAgro.Model
 {
-    [Table("Rebanho")]
+    // Representa a tabela 'Rebanho' no banco de dados
+    [Table("Rebanho")]  // Mapeia a classe para a tabela 'Rebanho' no banco de dados
     public class RebanhoModel
     {
-        [Key] // Define como chave primária
-        [Column("IdRebanho")]
+        // Identificador único para o rebanho
+        [Key]  // Define a propriedade 'Id' como chave primária
+        [Column("IdRebanho")]  // Mapeia a propriedade 'Id' para a coluna 'IdRebanho' no banco de dados
         public int Id { get; set; }
 
-        [Column("Tipo")]
+        // Tipo do rebanho (ex: gado de corte, gado leiteiro, etc.)
+        [Column("Tipo")]  // Mapeia a propriedade 'Tipo' para a coluna 'Tipo' no banco de dados
         public string Tipo { get; set; }
 
-        [Column("Destino")]
+        // Destino do rebanho (ex: abate, reprodução, venda, etc.)
+        [Column("Destino")]  // Mapeia a propriedade 'Destino' para a coluna 'Destino' no banco de dados
         public string Destino { get; set; }
 
-        [Column("fk_Animal_CodigoBrinco")]
+        // Código de identificação do animal (chave estrangeira para a tabela 'Animal')
+        [Column("fk_Animal_CodigoBrinco")]  // Mapeia a chave estrangeira 'CodigoBrinco' para a coluna 'fk_Animal_CodigoBrinco'
         public int CodigoBrinco { get; set; }
-        public virtual AnimalModel Animal { get; set; }
+
+        // Relacionamento de navegação com o modelo 'AnimalModel'
+        public virtual AnimalModel Animal { get; set; }  // Permite navegar para o animal associado
     }
 }
