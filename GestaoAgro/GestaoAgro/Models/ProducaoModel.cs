ad94520401e3e0d74822444122e3a08df9ebaf4c
LM <100394745+LhuanyMotta@users.noreply.github.com>
2024-12-16 11:42:32 -0400

    Update ProducaoModel.cs

--- GestaoAgro/GestaoAgro/Models/ProducaoModel.cs
+++ GestaoAgro/GestaoAgro/Models/ProducaoModel.cs
@@ -1,26 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoAgro.Model
 {
-    [Table("Producao")]
+    // Representa a tabela 'Producao' no banco de dados
+    [Table("Producao")]  // Mapeia a classe para a tabela 'Producao' no banco de dados
     public class ProducaoModel
     {
-        [Key] // Define como chave primária
-        [Column("Id")]
+        // Identificador único da produção (chave primária)
+        [Key]  // Define como chave primária
+        [Column("Id")]  // Mapeia a propriedade 'Id' para a coluna 'Id' no banco de dados
         public int Id { get; set; }
 
-        [Column("TipoProducao")]
+        // Tipo de produção realizada (ex: leite, carne, etc.)
+        [Column("TipoProducao")]  // Mapeia a propriedade 'TipoProducao' para a coluna 'TipoProducao'
         public string TipoProducao { get; set; }
 
-        [Column("Data")]
+        // Data da produção (quando a produção foi realizada)
+        [Column("Data")]  // Mapeia a propriedade 'Data' para a coluna 'Data'
         public DateTime Data { get; set; }
 
-        [Column("QuantidadeProduzida")]
+        // Quantidade produzida (ex: quantidade de leite ou carne)
+        [Column("QuantidadeProduzida")]  // Mapeia a propriedade 'QuantidadeProduzida' para a coluna 'QuantidadeProduzida'
         public string QuantidadeProduzida { get; set; }
 
-        [Column("fk_Animal_CodigoBrinco")]
+        // Código do brinco do animal associado à produção
+        [Column("fk_Animal_CodigoBrinco")]  // Mapeia a chave estrangeira para a coluna 'fk_Animal_CodigoBrinco'
         public int CodigoBrinco { get; set; }
-        public virtual AnimalModel Animal { get; set; }
+
+        // Relacionamento com o modelo 'AnimalModel' através do código de brinco
+        public virtual AnimalModel Animal { get; set; }  // Relacionamento de navegação
     }
 }
