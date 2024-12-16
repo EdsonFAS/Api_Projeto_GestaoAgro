87273efe6a6c29278f41710e00f8eaaa6d9cdbe8
LM <100394745+LhuanyMotta@users.noreply.github.com>
2024-12-16 11:42:17 -0400

    Update PastagemModel.cs

--- GestaoAgro/GestaoAgro/Models/PastagemModel.cs
+++ GestaoAgro/GestaoAgro/Models/PastagemModel.cs
@@ -1,25 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoAgro.Model
 {
-    [Table("Pastagem")]
+    // Representa a tabela 'Pastagem' no banco de dados
+    [Table("Pastagem")]  // Mapeia a classe para a tabela 'Pastagem' no banco de dados
     public class PastagemModel
     {
-        [Column("Id")]
+        // Identificador único da pastagem
+        [Column("Id")]  // Define o nome da coluna correspondente no banco de dados
         public int Id { get; set; }
 
-        [Column("AreaPastagem")]
+        // Área da pastagem em hectares ou outra unidade de medida
+        [Column("AreaPastagem")]  // Define o nome da coluna correspondente no banco de dados
         public double AreaPastagem { get; set; }
 
-        [Column("LocalizacaoPastagem")]
+        // Localização geográfica da pastagem (ex: coordenadas ou nome do campo)
+        [Column("LocalizacaoPastagem")]  // Define o nome da coluna correspondente no banco de dados
         public string LocalizacaoPastagem { get; set; }
 
-        [Column("Periodo")]
+        // Período de tempo relacionado à pastagem (ex: mês ou estação do ano)
+        [Column("Periodo")]  // Define o nome da coluna correspondente no banco de dados
         public int Periodo { get; set; }
 
-        [Column("fk_Animal_CodigoBrinco")]
+        // Código do brinco do animal associado a essa pastagem
+        [Column("fk_Animal_CodigoBrinco")]  // Define o nome da coluna correspondente no banco de dados
         public int CodigoBrinco { get; set; }
-        public virtual AnimalModel Animal { get; set; }
+
+        // Relacionamento com o modelo 'AnimalModel' através do código de brinco
+        public virtual AnimalModel Animal { get; set; }  // Relacionamento de navegação
     }
 }
